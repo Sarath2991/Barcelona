71900ba22070251302cca988cecaaa274ae79aa3
Sarath Ravichandran <Sarath.Ravichandran@emishealth.com>
2019-12-26 13:38:41 +0530

    Updates

---
+++ precache-manifest.200d60e91fb47296d2f811979248726e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf73bc51918bf99129ba966cfc2da1d",
+    "url": "/Barcelona/index.html"
+  },
+  {
+    "revision": "e578a2b49f5cadb5d329",
+    "url": "/Barcelona/static/css/main.41e4e6f1.chunk.css"
+  },
+  {
+    "revision": "c09eaa5856a2ef8f48ed",
+    "url": "/Barcelona/static/js/2.1746da33.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Barcelona/static/js/2.1746da33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e578a2b49f5cadb5d329",
+    "url": "/Barcelona/static/js/main.eeceb5f3.chunk.js"
+  },
+  {
+    "revision": "3cf22520c218cb483d1c",
+    "url": "/Barcelona/static/js/runtime-main.be482422.js"
+  }
+]);
